b1d15ae0c4a401a62f9facc2dd4b5e622a0e8076
Brian Pittman <shweazel@gmail.com>
2013-01-27 09:20:08 -0600

    Initial activity w/sherlock

---
+++ src/com/example/anr_pad/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.anr_pad;
+
+import com.actionbarsherlock.app.SherlockActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends SherlockActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
