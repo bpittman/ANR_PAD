737e17c76856845489970287fa72a838d137c381
Brian Pittman <shweazel@gmail.com>
2013-01-27 09:53:09 -0600

    Add some empty fragments

--- src/com/example/anr_pad/MainActivity.java
+++ src/com/example/anr_pad/MainActivity.java
@@ -8,6 +8,9 @@ import android.support.v4.app.FragmentTransaction;
 
 public class MainActivity extends SherlockFragmentActivity  implements ActionBar.TabListener {
 
+    private RoundedColourFragment leftFrag;
+    private RoundedColourFragment rightFrag;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -21,6 +24,19 @@ public class MainActivity extends SherlockFragmentActivity  implements ActionBar
         ab.addTab(ab.newTab().setText("Runner").setTabListener(this));
         ab.addTab(ab.newTab().setText("Corp").setTabListener(this));
         showTabsNav();
+
+        // create a couple of simple fragments as placeholders
+        final int MARGIN = 16;
+        leftFrag = new RoundedColourFragment(getResources().getColor(
+                R.color.android_green), 1f, MARGIN, MARGIN / 2, MARGIN, MARGIN);
+        rightFrag = new RoundedColourFragment(getResources().getColor(
+                R.color.honeycombish_blue), 2f, MARGIN / 2, MARGIN, MARGIN,
+                MARGIN);
+
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.add(R.id.root, leftFrag);
+        ft.add(R.id.root, rightFrag);
+        ft.commit();
 	}
 
     private void showTabsNav() {
